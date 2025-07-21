a3235d8ef355def4dfe3dbb9ccf64ff08977dea0
KhalelMishaal2 <25080409@imail.sunway.edu.my>
2025-07-21 06:01:38 +0000

    work day one

---
+++ Part1/ex03.py
@@ -0,0 +1,8 @@
+# Original text
+string1 = 'MaRviN THE bOt'
+# Print results
+print(string1)
+print(string1.lower())
+print(string1.upper())
+string_nospace = "".join(string1.split())
+print(string_nospace)
---
+++ Part1/ex05.py
@@ -0,0 +1,4 @@
+text1 = "I am"
+text2 = " a moderately"
+text3 = " ok person"
+print (text1 + text2 + text3)
