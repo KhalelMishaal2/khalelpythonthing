b98e2aca8682653c0291efd2396ee10c75828ea2
KhalelMishaal2 <25080409@imail.sunway.edu.my>
2025-07-22 03:26:04 +0000

    before break

--- Part2/ex13.py
+++ Part2/ex13.py
@@ -6,5 +6,11 @@ count = 0
 # Stop the loop when count reaches 5
 
 # Insert your code here
+def count_to_five():
+    count = 0
+    while count < 5:
+        print(count)
+        count += 1
 
+count_to_five()
 # Don't forget to increase count each loop!
